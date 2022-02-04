ee561947ce0759e3cb1262b19d40b94cee949074
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2022-02-04 11:55:55 +0100

    Dit hoort bij opdracht: F1.7.02.O2 - Een lichtschakelaar: JavaScript

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ document.body.appendChild(btn);
 
 // schijf hier tussen je code
 
+var lightstatus = "off";
+document.body.style.backgroundColor = "black";
+btn.innerHTML = 'Switch light on';
 
+function lightswitch() {
+    if (lightstatus == "off") {
+        btn.innerHTML = "Switch light off";
+        lightstatus = "on";
+        document.body.style.backgroundColor = "yellow";
+        console.log("Light is on")
+    } 
+    else if (lightstatus == "on") {
+        btn.innerHTML = "Switch light on";
+        lightstatus = "off";
+        document.body.style.backgroundColor = "black";
+        console.log("Light is off")
+    }
+}
+
+btn.onclick = lightswitch
 
 // schijf hier tussen je code
